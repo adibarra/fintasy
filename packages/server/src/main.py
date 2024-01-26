e46909cd5cf52ae7ce82e8c2ac2921628c8625ee
adibarra <93070681+adibarra@users.noreply.github.com>
2024-01-26 08:33:33 +0000

    formatting and misc

--- packages/server/src/main.py
+++ packages/server/src/main.py
@@ -1,7 +1,7 @@
 from config import API_HOST, API_PORT, API_ROUTES_DIR, API_STATIC_DIRS
 
 #
-# main Python Flask (or FastAPI?) code goes here
+# Main Python Flask (or FastAPI?) code goes here
 #
 
 print('Server should be running on: http://'+API_HOST+':'+str(API_PORT))
--- packages/server/src/routes/README.md
+++ packages/server/src/routes/README.md
@@ -1,1 +1,1 @@
-This directory should be used to store the routes that will be used by the Flask backend.
+This directory should be used to store the routes that will be used by the API backend.
