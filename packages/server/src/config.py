e78d27ebd3a98fc743e2d0889dc0482d5d383028
adibarra <93070681+adibarra@users.noreply.github.com>
2024-01-25 18:25:39 -0600

    Update project scripts and server configuration

--- packages/server/src/config.py
+++ packages/server/src/config.py
@@ -2,7 +2,7 @@ import os
 import sys
 from typing import List
 
-IS_PRODUCTION: bool = '-prod' in sys.argv
+IS_PRODUCTION: bool = bool(set(['--prod', '--production']) & set(sys.argv))
 API_HOST: str = 'localhost'
 API_PORT: int = 3000 if IS_PRODUCTION else 3332
 API_ROUTES_DIR: str = os.path.join('src', 'routes')
