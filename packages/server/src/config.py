50826839547b94ac97fdb3fe1eb8a2e65abda7b4
adibarra <93070681+adibarra@users.noreply.github.com>
2024-01-26 10:00:39 +0000

    run linter

--- packages/server/src/config.py
+++ packages/server/src/config.py
@@ -2,10 +2,10 @@ import os
 import sys
 from typing import List
 
-IS_PRODUCTION: bool = bool(set(['--prod', '--production']) & set(sys.argv))
-API_HOST: str = 'localhost'
+IS_PRODUCTION: bool = bool(set(["--prod", "--production"]) & set(sys.argv))
+API_HOST: str = "localhost"
 API_PORT: int = 3000 if IS_PRODUCTION else 3332
-API_ROUTES_DIR: str = os.path.join('src', 'routes')
-API_STATIC_DIRS: List[str] = ['public']
+API_ROUTES_DIR: str = os.path.join("src", "routes")
+API_STATIC_DIRS: List[str] = ["public"]
 if IS_PRODUCTION:
-    API_STATIC_DIRS.append(os.path.join('..', 'client', 'dist'))
+    API_STATIC_DIRS.append(os.path.join("..", "client", "dist"))
--- packages/server/src/main.py
+++ packages/server/src/main.py
@@ -4,6 +4,6 @@ from config import API_HOST, API_PORT, API_ROUTES_DIR, API_STATIC_DIRS
 # Main Python Flask (or FastAPI?) code goes here
 #
 
-print('Server should be running on: http://'+API_HOST+':'+str(API_PORT))
-print('Server should be serving the api from:', API_ROUTES_DIR)
-print('Server should also be statically serving from:', API_STATIC_DIRS)
+print("Server should be running on: http://" + API_HOST + ":" + str(API_PORT))
+print("Server should be serving the api from:", API_ROUTES_DIR)
+print("Server should also be statically serving from:", API_STATIC_DIRS)
