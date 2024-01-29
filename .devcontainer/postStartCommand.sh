efb2003b6f0e21f3eb8df2270fd6a7a696676f0a
adibarra <93070681+adibarra@users.noreply.github.com>
2024-01-29 22:04:36 +0000

    only install lts if needed

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,6 +1,5 @@
-. /usr/local/share/nvm/nvm.sh
-nvm install
-nvm use
+. ${NVM_DIR}/nvm.sh
+nvm use || nvm install --lts
 corepack enable
 corepack prepare pnpm@latest --activate
 printf '\n\n%b' "\033[34;1mAll set up. You are good to go!\033[0m\n"
