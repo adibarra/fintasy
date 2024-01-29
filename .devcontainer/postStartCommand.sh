255cefe3d53f48d5a6490813b0b00e2ee514a301
adibarra <93070681+adibarra@users.noreply.github.com>
2024-01-29 22:07:53 +0000

    improve setup complete message

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -2,5 +2,5 @@
 nvm use || nvm install --lts
 corepack enable
 corepack prepare pnpm@latest --activate
-printf '\n\n%b' "\033[34;1mAll set up. You are good to go!\033[0m\n"
+printf '\n\n%b' "\033[34;1mAll scripts completed! Environment is now set up.\033[0m\n"
 $( exit 0 )
