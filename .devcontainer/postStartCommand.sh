fe0cf9061913f25b03e895697e2f079a5726ccb7
adibarra <93070681+adibarra@users.noreply.github.com>
2024-01-30 19:24:58 +0000

    manually set pnpm-store path

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -2,5 +2,6 @@
 nvm use || nvm install --lts
 corepack enable
 corepack prepare pnpm@latest --activate
+pnpm config set store-dir $HOME/.pnpm-store
 printf '\n\n%b' "\033[34;1mAll scripts completed! Environment is now set up.\033[0m\n"
 $( exit 0 )
