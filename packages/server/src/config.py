1c01784bf87ca140589344b9bfad4128b5852fd3
adibarra <93070681+adibarra@users.noreply.github.com>
2024-02-14 20:01:53 +0000

    add comments

--- packages/server/src/config.py
+++ packages/server/src/config.py
@@ -2,9 +2,12 @@ import os
 import sys
 from typing import List
 
+# Set configuration variables
 IS_PRODUCTION: bool = bool(set(["--prod", "--production"]) & set(sys.argv))
 API_HOST: str = "localhost"
 API_PORT: int = 3000 if IS_PRODUCTION else 3332
+
+# Set routes and static directories
 API_ROUTES_DIR: str = os.path.join("src", "routes")
 API_STATIC_DIRS: List[str] = ["public"]
 if IS_PRODUCTION:
