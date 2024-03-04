fd39b5de965be8230c7cfb8e7feda340e3930dcf
adibarra <93070681+adibarra@users.noreply.github.com>
2024-03-04 20:23:53 +0000

    add breadcrumbs to navbar

--- packages/client/src/components/layout/NavBar.vue
+++ packages/client/src/components/layout/NavBar.vue
@@ -1,6 +1,33 @@
+<script setup lang="ts">
+const { t } = useI18n()
+const route = useRoute()
+
+const breadcrumbs = computed(() => {
+  const parts = route.path.split('/').filter(Boolean)
+
+  let path = ''
+  return parts.map((part: string) => {
+    path += `/${part}`
+    return {
+      label: t(`pages.${path.replaceAll('/', '.').slice(1)}.title`),
+      key: path,
+    }
+  })
+})
+</script>
+
 <template>
   <n-layout-header bordered h-45px flex items-center>
     <div w-60 />
+    <n-breadcrumb>
+      <template v-for="crumb in breadcrumbs" :key="crumb">
+        <n-breadcrumb-item>
+          <router-link :to="crumb.key">
+            {{ crumb.label }}
+          </router-link>
+        </n-breadcrumb-item>
+      </template>
+    </n-breadcrumb>
     <div />
   </n-layout-header>
 </template>
