14c7a134499c46bbc21813ab2f569c1af14b6ad7
adibarra <93070681+adibarra@users.noreply.github.com>
2024-03-04 20:21:46 +0000

    fix items from both menus being selected at same time

--- packages/client/src/components/layout/SideBar.vue
+++ packages/client/src/components/layout/SideBar.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { type MenuOption, NIcon } from 'naive-ui'
+import { NIcon } from 'naive-ui'
+import type { MenuInst, MenuOption } from 'naive-ui'
 import {
   Dashboard as DashboardIcon,
   Help as HelpIcon,
@@ -12,22 +13,41 @@ import {
 import { useSidebarStore } from '~/stores/sidebar'
 
 const { t } = useI18n()
+const route = useRoute()
 const store = useSidebarStore()
 const menuOptions1: MenuOption[] = [
   { label: `${t('pages.dashboard.title')}`, key: '/dashboard', icon: renderIcon(DashboardIcon) },
-  { label: `${t('pages.trade.title')}`, key: '/dashboard/trade', icon: renderIcon(TradeIcon) },
-  { label: `${t('pages.tournaments.title')}`, key: '/dashboard/tournaments', icon: renderIcon(TournamentIcon) },
+  { label: `${t('pages.dashboard.trade.title')}`, key: '/dashboard/trade', icon: renderIcon(TradeIcon) },
+  { label: `${t('pages.dashboard.tournaments.title')}`, key: '/dashboard/tournaments', icon: renderIcon(TournamentIcon) },
 ]
 
 const menuOptions2: MenuOption[] = [
   { key: 'divider-1', type: 'divider' },
-  { label: `${t('pages.help.title')}`, key: '/dashboard/help', icon: renderIcon(HelpIcon) },
-  { label: `${t('pages.settings.title')}`, key: '/dashboard/settings', icon: renderIcon(SettingsIcon) },
+  { label: `${t('pages.dashboard.help.title')}`, key: '/dashboard/help', icon: renderIcon(HelpIcon), children: [
+    { label: `${t('pages.dashboard.help.index.title')}`, key: '/dashboard/help', icon: renderIcon(HelpIcon) },
+    { label: `${t('pages.dashboard.help.faq.title')}`, key: '/dashboard/help/faq', icon: renderIcon(HelpIcon) },
+  ] },
+  { label: `${t('pages.dashboard.settings.title')}`, key: '/dashboard/settings', icon: renderIcon(SettingsIcon) },
 ]
 
 function renderIcon(icon: Component) {
   return () => h(NIcon, null, { default: () => h(icon) })
 }
+
+const menu1SelectedKey = ref('')
+const menu2SelectedKey = ref('')
+const menu1 = ref<MenuInst | null>(null)
+const menu2 = ref<MenuInst | null>(null)
+function selectAndExpand(key: string) {
+  menu1SelectedKey.value = key
+  menu2SelectedKey.value = key
+  menu1.value?.showOption(key)
+  menu2.value?.showOption(key)
+}
+
+watch(() => route.path, () => {
+  selectAndExpand(route.path)
+})
 </script>
 
 <template>
@@ -43,8 +63,9 @@ function renderIcon(icon: Component) {
       <div min-h-0 flex-1>
         <n-scrollbar>
           <n-menu
+            ref="menu1"
+            v-model:value="menu1SelectedKey"
             :options="menuOptions1"
-            :root-indent="18"
             @update:value="(key: string) => {
               $router.push(key)
             }"
@@ -52,6 +73,8 @@ function renderIcon(icon: Component) {
         </n-scrollbar>
       </div>
       <n-menu
+        ref="menu2"
+        v-model:value="menu2SelectedKey"
         :options="menuOptions2"
         @update:value="(key: string) => {
           $router.push(key)
