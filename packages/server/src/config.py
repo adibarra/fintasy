99649489936dffe0ba00db874e28f340cbbea5c3
adibarra <93070681+adibarra@users.noreply.github.com>
2024-03-14 08:56:25 +0000

    add dotenv for backend

--- packages/server/src/config.py
+++ packages/server/src/config.py
@@ -1,14 +1,20 @@
 import os
 import sys
 from typing import List
+from dotenv import load_dotenv
 
-# Set configuration variables
+# check if running in production mode
 IS_PRODUCTION: bool = bool(set(["--prod", "--production"]) & set(sys.argv))
-API_HOST: str = "localhost"
-API_PORT: int = 3000 if IS_PRODUCTION else 3332
 
-# Set routes and static directories
-API_ROUTES_DIR: str = os.path.join("src", "routes")
-API_STATIC_DIRS: List[str] = ["public"]
+# load environment variables
 if IS_PRODUCTION:
-    API_STATIC_DIRS.append(os.path.join("..", "client", "dist"))
+    if not load_dotenv(dotenv_path=os.path.join("..", "..", ".env.production")):
+        print("Failed to load environment vars... Does '.env.production' exist?")
+        sys.exit(1)
+
+# server configuration
+API_HOST: str = os.environ.get("SERVER_API_HOST") if IS_PRODUCTION else "localhost"
+API_PORT: int = os.environ.get("SERVER_API_PORT") if IS_PRODUCTION else 3332
+API_CORS_ORIGINS: List[str] = os.environ.get("SERVER_API_CORS_ORIGINS").split(",") if IS_PRODUCTION else ["*"]
+API_STATIC_DIRS: List[str] = [os.path.join("..", "client", "dist")] if IS_PRODUCTION else []
+API_ROUTES_DIR: str = os.path.join("src", "routes")
