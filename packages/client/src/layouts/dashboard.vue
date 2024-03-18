612ae4341eb3912756cc5f939037d9148f6f8569
adibarra <93070681+adibarra@users.noreply.github.com>
2024-03-18 00:33:06 +0000

    fix scroll area

--- packages/client/src/layouts/dashboard.vue
+++ packages/client/src/layouts/dashboard.vue
@@ -4,8 +4,10 @@
     <n-layout has-sider :style="{ height: 'calc(100% - 48px)' }">
       <SideBar />
       <n-layout-content>
-        <div h-full flex grow flex-col bg--c-bg-primary p-4 text--c-text>
-          <RouterView />
+        <div h-full flex grow flex-col bg--c-bg-primary text--c-text>
+          <n-scrollbar p-4>
+            <RouterView />
+          </n-scrollbar>
         </div>
       </n-layout-content>
     </n-layout>
