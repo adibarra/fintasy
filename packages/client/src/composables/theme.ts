2f2b426dcd69239b5e724a888ce73a0e9ce7ba74
adibarra <93070681+adibarra@users.noreply.github.com>
2024-03-25 13:58:03 +0000

    update theme composable name

--- packages/client/src/composables/theme.ts
+++ packages/client/src/composables/theme.ts
@@ -1,12 +1,12 @@
 /**
  * @author: adibarra (Alec Ibarra)
- * @description: Composable for handling dark mode state
+ * @description: Composable for handling color theme state
  */
 
 export const preferredDark = usePreferredDark()
 export const isDark = useDark({
   initialValue: 'dark',
-  storageKey: 'color-scheme',
+  storageKey: 'color-theme',
 })
 export const toggleTheme = useToggle(isDark)
 export const colorTheme = computed(() => (isDark.value ? 'dark' : 'light'))
