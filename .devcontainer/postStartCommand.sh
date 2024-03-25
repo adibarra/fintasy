a0c1230e244fa9535af112655626eb9775f2a771
adibarra <93070681+adibarra@users.noreply.github.com>
2024-03-25 13:58:20 +0000

    improve devcontainer

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -7,5 +7,5 @@ corepack enable
 corepack prepare pnpm@latest --activate
 pnpm config set store-dir $HOME/.pnpm-store
 cp --no-clobber .env.production.example .env.production
-printf '\n\n%b' "\033[34;1mAll scripts completed! Environment is now set up.\033[0m\n"
+clear
 $( exit 0 )
