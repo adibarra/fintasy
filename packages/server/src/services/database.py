264e3cade9046146e9a9006b48b5a4c5ae0dbb72
adibarra <93070681+adibarra@users.noreply.github.com>
2024-03-27 20:41:20 +0000

    add stub for database singleton

--- packages/server/src/services/database.py
+++ packages/server/src/services/database.py
@@ -1,1 +1,14 @@
-# database handling goes here
+# @author: Alec Ibarra
+# @description: Database class for handling database interactions
+
+
+class Database(object):
+    def __new__(cls):
+        if not hasattr(cls, "instance"):
+            cls.instance = super(Database, cls).__new__(cls)
+        return cls.instance
+
+    def create_user(self, username: str, email: str, password_hash: str) -> bool:
+        # check if user with email already exists
+        # else create user
+        pass
