138e862eb866e248d8fd949c602ccf90da9f857e
adibarra <93070681+adibarra@users.noreply.github.com>
2024-03-27 14:39:48 +0000

    add testing framework for python

---
+++ packages/server/tests/basic_test.py
@@ -0,0 +1,12 @@
+# @author: Alec Ibarra (adibarra)
+# @description: Example test
+
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('test'.upper(), 'TEST')
+
+if __name__ == '__main__':
+    unittest.main()
