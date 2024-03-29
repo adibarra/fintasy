6c54ad1d2e0a941accef104d1617cf456b4f4b09
caleb-j-kim <coolycaleb@gmail.com>
2024-03-29 18:29:26 +0000

    Added requested functionality along with new functionality that allows users to create portfolios with default names.

--- packages/server/src/helpers/portfolio.py
+++ packages/server/src/helpers/portfolio.py
@@ -3,12 +3,13 @@
 
 from enum import Enum
 
+
 class Portfolio:
     """
-Create objects that will be sent to portfolio dashboard in the front end.
-*Portfolio Name: (Allow users to create new portfolios as long as the name of a portfolio doesn't already exist when the user requests to name their new portfolio it & allow for default portfolio name [Portfolio #])
-*Column Names: (Company Name, QTY / Quantity, Unit Price, Daily P/L, Total P/L)
-*Row Attributes: (Company Name, QTY / Quantity, Unit Price, Daily P/L, Total P/L) #replace with API
+    Create objects that will be sent to portfolio dashboard in the front end.
+    *Portfolio Name: (Allow users to create new portfolios as long as the name of a portfolio doesn't already exist when the user requests to name their new portfolio it & allow for default portfolio name [Portfolio #])
+    *Column Names: (Company Name, QTY / Quantity, Unit Price, Daily P/L, Total P/L)
+    *Row Attributes: (Company Name, QTY / Quantity, Unit Price, Daily P/L, Total P/L) #replace with API
     """
 
     PORTFOLIO_VALID_CHARACTERS = (
@@ -18,7 +19,7 @@ Create objects that will be sent to portfolio dashboard in the front end.
     PORTFOLIO_LENGTH_MAX = 20
 
     ERROR_TYPES = Enum(
-   """
+        """
         "ERROR_TYPES",
         [
             "PORTFOLIO_SHORT",
@@ -29,7 +30,6 @@ Create objects that will be sent to portfolio dashboard in the front end.
    """
     )
 
-
     ERROR_MESSAGES = {
         ERROR_TYPES.PORTFOLIO_SHORT: "Portfolio name must be at least 1 character long",
         ERROR_TYPES.PORTFOLIO_LONG: "Portfolio name must be less than or equal to 20 characters long",
@@ -37,15 +37,35 @@ Create objects that will be sent to portfolio dashboard in the front end.
         ERROR_TYPES.PORTFOLIO_NAME_EXISTS: "Portfolio name must be unique",
     }
 
-
     def __init__(self):
         """
         Create a dictionary of portfolios that house the portfolio name and its attributes
         """
         self.portfolios = {}
 
+    def default_name(self):
+        """
+        Args:
+            user_input: Must be empty but not null in order for a default portfolio name to be created.
 
-    def validate_portfolioname(self, portfolio_name: str):
+        Returns:
+            Default portfolio name that's unique & it's stored inside of the self dictionary.
+        """
+        user_input = input(
+            "Enter a name for your portfolio.\n(If you would like a default name then please press Enter.)"
+        )
+        if user_input == "":
+            i = 0
+            while True:
+                portfolio_name = f"Portfolio #{i}"
+                if portfolio_name in self.portfolios:
+                    i = i + 1
+                else:
+                    self.portfolios[portfolio_name] = portfolio_name
+                    break
+        return self.portfolios[portfolio_name]
+
+    def validate_portfolio_name(self, portfolio_name: str):
         """
         Validates the given portfolio name.
 
@@ -64,22 +84,27 @@ Create objects that will be sent to portfolio dashboard in the front end.
         """
 
         if len(portfolio_name) < Portfolio.PORTFOLIO_LENGTH_MIN:
-            raise ValueError(Portfolio.ERROR_MESSAGES[Portfolio.ERROR_TYPES.PORTFOLIO_SHORT])
+            raise ValueError(
+                Portfolio.ERROR_MESSAGES[Portfolio.ERROR_TYPES.PORTFOLIO_SHORT]
+            )
 
         if len(portfolio_name) > Portfolio.PORTFOLIO_LENGTH_MAX:
-            raise ValueError(Portfolio.ERROR_MESSAGES[Portfolio.ERROR_TYPES.PORTFOLIO_LONG])
+            raise ValueError(
+                Portfolio.ERROR_MESSAGES[Portfolio.ERROR_TYPES.PORTFOLIO_LONG]
+            )
 
         for char in portfolio_name:
             if char not in Portfolio.PORTFOLIO_VALID_CHARACTERS:
                 raise ValueError(
-                    Portfolio.ERROR_MESSAGES[Portfolio.ERROR_TYPES.PORTFOLIO_INVALID_CHARACTERS]
+                    Portfolio.ERROR_MESSAGES[
+                        Portfolio.ERROR_TYPES.PORTFOLIO_INVALID_CHARACTERS
+                    ]
                 )
 
         if portfolio_name in self.portfolios:
             raise ValueError("Portfolio name must be unique.")
         self.portfolios[portfolio_name] = portfolio_name
         return True
-
 
     def remove_portfolio(self, portfolio_name: str):
         """
@@ -94,12 +119,21 @@ Create objects that will be sent to portfolio dashboard in the front end.
         """
 
         if portfolio_name not in self.portfolios:
-            raise KeyError("Portfolio does not exist.\nPlease try again and input a portfolio name that exists inside of the Fintasy Databases.")
+            raise KeyError(
+                "Portfolio does not exist.\nPlease try again and input a portfolio name that exists inside of the Fintasy Databases."
+            )
         del self.portfolios[portfolio_name]
         return True
 
-
-    def add_data(self, portfolio_name: str, company_name: str, qty: int, unit_price: float, daily_PL: float, total_PL: float): #unit_price, dailyPL, and totalPL will be received through API later
+    def add_data(
+        self,
+        portfolio_name: str,
+        company_name: str,
+        qty: int,
+        unit_price: float,
+        daily_PL: float,
+        total_PL: float,
+    ):  # unit_price, dailyPL, and totalPL will be received through API later
         """
         Args:
             portfolio_name (str): The portfolio name must exist inside of the dictionary.
@@ -118,35 +152,97 @@ Create objects that will be sent to portfolio dashboard in the front end.
             Updated self.portfolios[portfolio_name] dictionary with updated values for key data.
         """
         if portfolio_name not in self.portfolios:
-            raise KeyError("Portfolio does not exist.\nPlease try again and input a portfolio name that exists inside of the Fintasy Databases.")
+            raise KeyError(
+                "Portfolio does not exist.\nPlease try again and input a portfolio name that exists inside of the Fintasy Databases."
+            )
 
         if len(company_name) < Portfolio.PORTFOLIO_LENGTH_MIN:
-            raise ValueError("Company name must contain one or more characters inside of its name.")
+            raise ValueError(
+                "Company name must contain one or more characters inside of its name."
+            )
 
         for char in company_name:
             if char not in Portfolio.PORTFOLIO_VALID_CHARACTERS:
                 raise ValueError("Company name must contain only valid characters.")
 
         if qty < 0:
-            raise ValueError(f"The quantity of {company_name}'s stock(s) must be greater than zero inside of this portfolio.")
+            raise ValueError(
+                f"The quantity of {company_name}'s stock(s) must be greater than zero inside of this portfolio."
+            )
 
         if unit_price < 0:
-            raise ValueError(f"The unit price of {company_name}'s stock(s) must be greater than zero inside of this portfolio.")
+            raise ValueError(
+                f"The unit price of {company_name}'s stock(s) must be greater than zero inside of this portfolio."
+            )
 
         if daily_PL < 0:
-            raise ValueError(f"The daily PL of {company_name}'s stock(s) must be greater than zero inside of this portfolio.")
+            raise ValueError(
+                f"The daily PL of {company_name}'s stock(s) must be greater than zero inside of this portfolio."
+            )
 
         if total_PL < 0:
-            raise ValueError(f"The daily PL of {company_name}'s stock(s) must be greater than zero inside of this portfolio.")
+            raise ValueError(
+                f"The daily PL of {company_name}'s stock(s) must be greater than zero inside of this portfolio."
+            )
 
         self.portfolios[portfolio_name][company_name] = {
-            'qty': qty,
-            'unit_price': unit_price,
-            'daily_PL': daily_PL,
-            'total_PL': total_PL
+            "qty": qty,
+            "unit_price": unit_price,
+            "daily_PL": daily_PL,
+            "total_PL": total_PL,
         }
         return True
 
+    def initialize_balance(self, portfolio_name: str):
+        """
+        Args:
+            portfolio_name (str): The portfolio must exist inside of the dictionary
+
+        Raises:
+            KeyError: If the portfolio name doesn't exist inside of the dictionary
+
+        Creates:
+            Initial balance of $500 USD inside of the portfolio.
+        """
+
+        if portfolio_name not in self.portfolios:
+            raise KeyError(
+                "Portfolio does not exist. \nPlease try again and input a portfolio name that exists inside of the Fintasy Databases."
+            )
+        self.portfolios[portfolio_name] = {"balance": 500}
+        return True
+
+    def change_balance(self, portfolio_name: str, balance: float, transaction: float):
+        """
+        Args:
+            portfolio_name (str): The portfolio must exist inside of the dictionary
+            transaction (float): The new must contain only valid float values or if it's greater than the portfolio's current balance.
+
+        Raises:
+            KeyError: If the portfolio name doesn't exist inside of the dictionary.
+            ValueError: If the transaction contains invalid values.
+
+        Returns:
+            Updated balance amount for this portfolio.
+        """
+
+        if portfolio_name not in self.portfolios:
+            raise KeyError(
+                "Portfolio does not exist.\nPlease try again and input a portfolio name that exists inside of the Fintasy Databases."
+            )
+
+        if transaction < 0:
+            raise ValueError("The transaction amount must be greater than $0.00.")
+
+        updated_balance = balance - transaction
+
+        if updated_balance < 0:
+            raise ValueError(
+                f"The transaction amount exceeds the {portfolio_name}'s balance. \nPlease try again"
+            )
+
+        self.portfolios[portfolio_name] = {"balance": updated_balance}
+        return self.portfolios[portfolio_name][balance]
 
     def get_portfolio(self, portfolio_name: str):
         """
@@ -161,9 +257,10 @@ Create objects that will be sent to portfolio dashboard in the front end.
         """
 
         if portfolio_name not in self.portfolios:
-            raise KeyError("Portfolio does not exist.\nPlease try again and input a portfolio name that exists inside of the Fintasy Databases.")
+            raise KeyError(
+                "Portfolio does not exist.\nPlease try again and input a portfolio name that exists inside of the Fintasy Databases."
+            )
         return self.portfolios[portfolio_name]
-
 
     def list_portfolios(self):
         """
