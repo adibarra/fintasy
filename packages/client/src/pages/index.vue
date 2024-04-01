da6fff31a8bbd8d41a44fbe94dedd2cd2b0064fa
adibarra <93070681+adibarra@users.noreply.github.com>
2024-04-01 20:49:27 +0000

    optimize css / html

--- packages/client/src/pages/index.vue
+++ packages/client/src/pages/index.vue
@@ -12,27 +12,20 @@ useHead({
 </script>
 
 <template>
-  <nav class="mx-10 my-4 flex justify-between lg:flex-row md:flex-row xl:flex-row sm:flex-col">
-    <router-link to="/" class="flex items-center justify-center">
+  <nav class="mx-6 my-4 flex">
+    <router-link to="/" class="flex items-center justify-center gap-2">
       <img
         src="/pwa-192x192.png" alt="Fintasy Logo"
-        class="mr-2 h-14 lg:mb-2 lg:mr-0 md:mb-2 md:mr-0 sm:mb-4 xl:mb-2 xl:mr-0"
+        class="h-14"
       >
-
-      <div class="pl-2 text-4xl lg:text-4xl md:text-3xl sm:text-2xl xl:text-4xl">
+      <div class="text-2xl lg:text-4xl md:text-3xl">
         Fintasy
       </div>
     </router-link>
-    <div class="flex items-center">
+    <div grow />
+    <div class="flex items-center gap-5">
       <button
-        class="ml-10 rd-10 px-4 py-2 text-lg hover:text--c-accent"
-        @click="$router.push('/dashboard')"
-      >
-        {{ t('pages.dashboard.title') }}
-      </button>
-
-      <button
-        class="ml-10 rd-10 bg--c-inverse hover:bg--c-accent px-4 py-2 text-lg text--c-bg"
+        class="rd-10 bg--c-inverse hover:bg--c-accent px-6 py-2 text-lg text--c-bg"
         @click="$router.push('/login')"
       >
         {{ t('pages.login.title') }}
@@ -40,41 +33,26 @@ useHead({
     </div>
   </nav>
 
-  <div class="my-25 flex flex-col-reverse items-center justify-center lg:flex-row md:flex-row sm:flex-row xl:flex-row">
-    <div class="mx-5 my-15 lg:mx-55 md:mx-15 sm:mx-10 xl:mx-55">
-      <div class="text-8xl text--c-accent -mr-50 lg:text-6xl md:text-5xl sm:text-4xl xl:text-8xl">
-        Take Your Stocks To The Next Level
-      </div>
-      <div class="py-10 text-lg lg:text-lg md:text-lg sm:text-base xl:text-lg">
-        Fintasy is a stock market simulator that allows you to trade stocks with fake money. Compete against your
-        friends and see who can make the most money!
+  <div class="h-full w-full flex items-center justify-center">
+    <div class="max-w-350 flex flex-col items-center justify-center xl:flex-row xl:flex-row-reverse">
+      <img
+        src="/images/card.png" alt="Fintasy Logo" class="m-15 h-48 lg:h-96 md:h-72"
+        style="filter: drop-shadow(8px 24px 24px rgba(5, 150, 105, .8));"
+      >
+      <div class="m-15">
+        <div class="text-4xl text--c-accent lg:text-6xl md:text-5xl">
+          Take Your Stocks To The Next Level
+        </div>
+        <div class="pt-10 text-base md:text-lg">
+          Fintasy is a stock market simulator that allows you to trade stocks with fake money. Compete against your
+          friends and see who can make the most money!
+        </div>
       </div>
     </div>
-
-    <img
-      src="/images/card.png" alt="Fintasy Logo" class="mr-5 h-96 lg:mr-55 md:mr-15 sm:mr-10 xl:mr-55"
-      style="filter: drop-shadow(8px 24px 24px rgba(5, 150, 105, .8));"
-    >
   </div>
-
-  <!-- Some spacers and a temporary footer -->
-  <span class="opacity-50">
-    <div class="h-10" />
-    [ pages/index.vue ]
-    <div class="h-10" />
-  </span>
 </template>
 
 <route lang="yaml">
   meta:
     layout: default
 </route>
-
-<style scoped>
-@media screen and (max-width: 640px) {
-  /* Small devices (phones) */
-  .flex-col-reverse {
-    flex-direction: column-reverse;
-  }
-}
-</style>
