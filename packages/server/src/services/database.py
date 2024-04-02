fcb347763fe627f20e739e53e278214fb22edd31
adibarra <93070681+adibarra@users.noreply.github.com>
2024-04-02 22:18:57 +0000

    fix duplicate internal class

--- packages/server/src/services/database.py
+++ packages/server/src/services/database.py
@@ -4,8 +4,7 @@
 from enum import Enum
 
 import psycopg2
-
-from src.config import POSTGRESQL_URI
+from config import POSTGRESQL_URI
 
 
 class Database(object):
@@ -29,60 +28,65 @@ class Database(object):
         ERROR_TYPES.USER_CREATION_FAILED: "Failed to create user",
     }
 
-    class Database:
-        def __new__(cls):
-            """
-            Creates a new instance of the Database class if it doesn't already exist.
+    def __new__(cls):
+        """
+        Creates a new instance of the Database class if it doesn't already exist.
 
-            Returns:
-                Database: The instance of the Database class.
-            """
+        Returns:
+            Database: The instance of the Database class.
+        """
 
-            if not hasattr(cls, "instance"):
-                cls.instance = super(Database, cls).__new__(cls)
-            return cls.instance
+        if not hasattr(cls, "instance"):
+            cls.instance = super(Database, cls).__new__(cls)
+        return cls.instance
 
-        def __init__(self):
-            """
-            This method creates necessary tables in the database if they don't already exist on startup.
-            """
-            try:
-                connection = psycopg2.connect(POSTGRESQL_URI)
-                cursor = connection.cursor()
+    def __init__(self):
+        """
+        This method creates necessary tables in the database if they don't already exist on startup.
+        """
 
-                # create tables here if they don't exist
+        connection = None
+        try:
+            print("Connecting to PostgreSQL database...")
+            connection = psycopg2.connect(POSTGRESQL_URI)
+            print("Connected to PostgreSQL database")
+            cursor = connection.cursor()
 
-                connection.commit()
+            # create tables here if they don't exist
 
-            except Exception as e:
-                print("Failed to connect to PostgreSQL database", e)
+            connection.commit()
 
-            finally:
-                if connection:
-                    cursor.close()
-                    connection.close()
+        except Exception as e:
+            print("Failed to connect to PostgreSQL database", e)
 
-        def create_user(self, username: str, email: str, password_hash: str) -> bool:
-            """
-            Attempts to create a new user in the database.
+        finally:
+            if connection:
+                cursor.close()
+                connection.close()
 
-            Returns:
-                bool: True if the user was created successfully, False otherwise.
-            """
-            try:
-                connection = psycopg2.connect(POSTGRESQL_URI)
-                cursor = connection.cursor()
+    def create_user(self, username: str, email: str, password_hash: str) -> bool:
+        """
+        Attempts to create a new user in the database.
 
-                # check if user with email already exists
-                # else create user
+        Returns:
+            bool: True if the user was created successfully, False otherwise.
+        """
 
-                connection.commit()
-                return True
+        connection = None
+        try:
+            connection = psycopg2.connect(POSTGRESQL_URI)
+            cursor = connection.cursor()
 
-            except Exception:
-                return False
+            # check if user with email already exists
+            # else create user
 
-            finally:
-                if connection:
-                    cursor.close()
-                    connection.close()
+            connection.commit()
+            return True
+
+        except Exception:
+            return False
+
+        finally:
+            if connection:
+                cursor.close()
+                connection.close()
