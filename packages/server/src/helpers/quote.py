c0d11d973c18df5cac32bc765070f8f48d3f9d4b
caleb-j-kim <coolycaleb@gmail.com>
2024-04-11 04:32:30 +0000

    Final updates to the routes branch(?)

--- packages/server/src/helpers/quote.py
+++ packages/server/src/helpers/quote.py
@@ -9,7 +9,7 @@ class Quote:
 
     INTERVALS = Enum("INTERVALS", ["MINUTE", "HOUR", "DAY", "WEEK", "MONTH", "YEAR"])
 
-    def __init__(self, symbol: str, price: float, timestamp: datetime):
+    def __init__(self, symbol: str, price: int, timestamp: datetime):
         """
         Initializes a Quote object.
 
