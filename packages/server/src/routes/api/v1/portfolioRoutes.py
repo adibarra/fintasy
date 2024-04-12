8d02b7ecfc1fff15d4b9841481e574898a501105
caleb-j-kim <coolycaleb@gmail.com>
2024-04-12 17:50:01 +0000

    fixed issues with portfolio routes

--- packages/server/src/routes/api/v1/portfolioRoutes.py
+++ packages/server/src/routes/api/v1/portfolioRoutes.py
@@ -4,7 +4,7 @@
 from datetime import datetime
 from typing import Optional
 
-from fastapi import APIRouter, Body, Depends, Header, HTTPException, Path, Query, status
+from fastapi import APIRouter, Body, Depends, Header, HTTPException, Path, status
 from helpers.portfolio import Portfolio
 from pydantic import UUID4, BaseModel
 from services.database import Database
@@ -36,6 +36,17 @@ class CreatePortfolioRequest(BaseModel):
 
 class UpdatePortfolioRequest(BaseModel):
     name: Optional[str] = None
+
+    class Config:
+        exclude_none = True
+
+
+class GetPortfoliosRequest(BaseModel):
+    owner: UUID4
+    tournament: Optional[UUID4] = None
+    name: Optional[str] = None
+    offset: Optional[int] = 0
+    limit: int
 
     class Config:
         exclude_none = True
@@ -92,34 +103,15 @@ def create_portfolio(
 ):
     token_owner = auth[0]
 
-    # TODO: only need to validate portfolio name
-    # but none of these functions are implemented
-    if not all(
-        [
-            Portfolio.validate_uuid(data.uuid),
-            Portfolio.validate_owner(data.owner),
-            Portfolio.validate_tournament(data.tournament),
-            Portfolio.validate_name(data.name),
-            Portfolio.validate_balance_cents(data.balance_cents),
-            Portfolio.validate_created_at(data.created_at),
-            Portfolio.validate_updated_at(data.updated_at),
-        ]
-    ):
+    if not all([Portfolio.validate_portfolio_name(data.name)]):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Bad Request",
         )
 
-    # TODO: check the parameters for create_portfolio
-    # the ones that are being passed in don't match the function signature
     portfolio = db.create_portfolio(
-        data.uuid,
-        data.owner,
         data.tournament,
         data.name,
-        data.balance_cents,
-        data.created_at,
-        data.updated_at,
     )
     if portfolio is None:
         raise HTTPException(
@@ -134,29 +126,26 @@ def create_portfolio(
     )
 
 
-# TODO: extract the query parameters into a separate class like GetPortfoliosRequest or something (tournament offset, limit are all optional)
 @router.get(
     "/portfolios", response_model=PortfolioResponse, status_code=status.HTTP_200_OK
 )
 def get_portfolios(
-    owner: UUID4 = Query(...),
-    tournament: UUID4 = Query(...),
-    name: str = Query(...),
-    offset: int = Query(...),
-    limit: int = Query(...),
+    data: GetPortfoliosRequest = Body(...),
     auth: tuple[UUID4, str] = Depends(authenticateToken),
 ):
     token_owner = auth[0]
 
     # Keep this check for now, might remove later with a more complex permission system
-    if owner != token_owner:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="Forbidden",
-        )
+    # if owner != token_owner:
+    # raise HTTPException(
+    # status_code=status.HTTP_403_FORBIDDEN,
+    # detail="Forbidden",
+    # )
 
-    # TODO: use db.get_portfolios() instead, and pass in the query parameters
-    portfolios = db.list_portfolios(uuid)
+    # TODO: use db.get_portfolios() instead, and pass in the query parameters (resolved?)
+    portfolios = db.get_portfolios(
+        data.owner, data.tournament, data.name, data.offset, data.limit, auth
+    )
     if portfolios is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
@@ -210,9 +199,8 @@ def update_portfolio(
             detail="Not Found",
         )
 
-    # TODO: Portfolio.validate name doesn't exist
     if data.name:
-        if not Portfolio.validate_name(data.name):
+        if not Portfolio.validate_portfolio_name(data.name):
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail="Bad Request",
@@ -241,8 +229,7 @@ def remove_portfolio(
             detail="Not Found",
         )
 
-    # TODO: db.delete_portfolio is probably what you want to use here
-    if not db.remove_portfolio(uuid):
+    if not db.delete_portfolio(portfolio_uuid):
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Internal Server Error",
