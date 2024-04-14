693acb1a9f257b775715f292320d293e9b77cbaf
adibarra <93070681+adibarra@users.noreply.github.com>
2024-04-14 21:12:36 +0000

    fix page titles

--- packages/client/src/pages/login.vue
+++ packages/client/src/pages/login.vue
@@ -8,6 +8,10 @@ import { useI18n } from 'vue-i18n'
 
 const { t } = useI18n()
 
+useHead({
+  title: `${t('pages.login.title')} • Fintasy`,
+})
+
 const username = ref('')
 const password = ref('')
 
