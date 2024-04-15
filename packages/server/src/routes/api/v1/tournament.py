e53d2abfa505c436641fa68ab09d96d9eb8480c2
adibarra <93070681+adibarra@users.noreply.github.com>
2024-04-15 13:24:51 +0000

    clean up tournament routes

---
+++ packages/server/src/routes/api/v1/tournament.py
@@ -0,0 +1,247 @@
+# @author: adibarra (Alec Ibarra), caleb-j-kim (Caleb Kim)
+# @description: Portfolio routes for the API
+
+from datetime import datetime
+from typing import List, Optional
+
+from fastapi import APIRouter, Body, Depends, Header, HTTPException, Path, Query, status
+from pydantic import UUID4, BaseModel
+
+from src.helpers.tournament import Tournament
+from src.services.database import Database
+
+db = Database()
+router = APIRouter(prefix="/api/v1")
+
+
+class TournamentData(BaseModel):
+    name: str
+    start_date: datetime
+    end_date: datetime
+    created_at: datetime
+    updated_at: datetime
+
+
+class CreateTournamentRequest(BaseModel):
+    name: str
+    start_date: datetime
+    end_date: datetime
+
+
+class UpdateTournamentRequest(BaseModel):
+    name: Optional[str] = None
+    start_date: Optional[datetime] = None
+    end_date: Optional[datetime] = None
+
+    class Config:
+        exclude_none = True
+
+
+class GetTournamentsRequest(BaseModel):
+    name: Optional[str] = None
+    owner: Optional[UUID4] = None
+    status: Optional[str] = None
+    start_date: Optional[datetime] = None
+    end_date: Optional[datetime] = None
+    offset: Optional[int] = None
+    limit: Optional[int] = None
+
+    class Config:
+        exclude_none = True
+
+
+class TournamentResponse(BaseModel):
+    code: int
+    message: str
+    data: Optional[TournamentData] = None
+
+    class Config:
+        exclude_none = True
+
+
+class TournamentsResponse(BaseModel):
+    code: int
+    message: str
+    data: Optional[List[TournamentData]] = None
+
+    class Config:
+        exclude_none = True
+
+
+async def authenticateToken(
+    authorization: str = Header(...),
+) -> tuple[UUID4, str]:
+    token = authorization.split(" ")[1]
+    token_owner = db.get_session(token)
+
+    # Validate the token exists
+    if token_owner is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Unauthorized",
+        )
+
+    return token_owner, token
+
+
+async def authenticate(
+    authorization: str = Header(...),
+    tournament_uuid: UUID4 = Path(...),
+) -> tuple[UUID4, str]:
+    token_owner, token = authenticateToken(authorization)
+
+    # Validate the token has permission for this resource
+    tournament = db.get_tournament(tournament_uuid)
+    if str(token_owner) != str(tournament.owner):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Forbidden",
+        )
+
+    return token_owner, token
+
+
+@router.post(
+    "/tournaments",
+    response_model=TournamentResponse,
+    status_code=status.HTTP_200_OK,
+)
+def create_tournament(
+    data: CreateTournamentRequest = Body(...),
+):
+    if not all(
+        [
+            Tournament.validate_name(data.name),
+            Tournament.validate_date(data.start_date),
+            Tournament.validate_end(data.end_date),
+        ]
+    ):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Bad Request",
+        )
+
+    tournaments = db.create_tournament(data)
+    return TournamentResponse(
+        code=200,
+        message="Ok",
+        data=tournaments,
+    )
+
+
+@router.get(
+    "/tournaments",
+    response_model=TournamentResponse,
+    status_code=status.HTTP_200_OK,
+)
+def get_tournaments(
+    data: GetTournamentsRequest = Query(...),
+    auth: tuple[UUID4, str] = Depends(authenticateToken),
+):
+    tournaments = db.get_tournaments(
+        uuid_owner=data.owner,
+        name=data.name,
+        status=data.status,
+        start_date=data.start_date,
+        end_date=data.end_date,
+        offset=data.offset,
+        limit=data.limit,
+    )
+    return TournamentsResponse(
+        code=200,
+        message="Ok",
+        data=tournaments,
+    )
+
+
+@router.get(
+    "/tournaments/{tournament_uuid}",
+    response_model=TournamentResponse,
+    status_code=status.HTTP_200_OK,
+)
+def get_tournament(
+    tournament_uuid: UUID4 = Path(...),
+    auth: tuple[UUID4, str] = Depends(authenticate),
+):
+    tournament = db.get_tournament(tournament_uuid)
+    if tournament is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Not Found",
+        )
+    return TournamentResponse(
+        code=200,
+        message="Ok",
+        data=tournament,
+    )
+
+
+@router.patch(
+    "/tournaments/{tournament_uuid}",
+    response_model=TournamentResponse,
+    status_code=status.HTTP_200_OK,
+)
+def update_tournament(
+    tournament_uuid: UUID4 = Path(...),
+    data: UpdateTournamentRequest = Body(...),
+    auth: tuple[UUID4, str] = Depends(authenticate),
+):
+    if not all(
+        [
+            Tournament.validate_name(data.name),
+            Tournament.validate_date(data.start_date),
+            Tournament.validate_end(data.end_date),
+        ]
+    ):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Bad Request",
+        )
+
+    tournament = db.get_tournament(tournament_uuid)
+    if tournament is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Not Found",
+        )
+
+    if not db.update_tournament(
+        tournament_uuid, data.name, data.start_date, data.end_date
+    ):
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Internal Server Error",
+        )
+
+    return TournamentResponse(
+        code=200,
+        message="Ok",
+    )
+
+
+@router.delete(
+    "/tournaments/{tournament_uuid}",
+    response_model=TournamentResponse,
+    status_code=status.HTTP_200_OK,
+)
+def delete_tournament(
+    tournament_uuid: UUID4 = Path(...),
+    auth: tuple[UUID4, str] = Depends(authenticate),
+):
+    tournaments = db.get_tournament(tournament_uuid)
+    if tournaments is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Not Found",
+        )
+
+    if not db.delete_tournament(tournament_uuid):
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Internal Server Error",
+        )
+
+    return TournamentResponse(
+        code=200,
+        message="Ok",
+    )
