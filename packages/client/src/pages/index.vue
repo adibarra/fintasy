07e470a02b83b710b3718a86d90ddca529704c01
adibarra <93070681+adibarra@users.noreply.github.com>
2024-04-17 17:38:23 +0000

    attributify css

--- packages/client/src/pages/index.vue
+++ packages/client/src/pages/index.vue
@@ -12,18 +12,20 @@ useHead({
 </script>
 
 <template>
-  <div class="h-10svh" />
-  <div class="flex items-center justify-center">
-    <div class="max-w-350 flex flex-col items-center justify-center xl:flex-row-reverse">
+  <div h-10svh />
+  <div flex items-center justify-center>
+    <div max-w-350 flex flex-col items-center justify-center xl:flex-row-reverse>
       <img
-        src="/images/card.png" alt="Fintasy Logo" class="m-15 h-36 md:h-72 sm:h-48"
+        src="/images/card.png"
+        alt="Fintasy Logo"
+        m-15 h-36 md:h-72 sm:h-48
         style="filter: drop-shadow(8px 24px 24px rgba(5, 150, 105, .8));"
       >
-      <div class="m-15">
-        <div class="text-4xl text--c-accent lg:text-6xl md:text-5xl">
+      <div m-15>
+        <div text-4xl text--c-accent lg:text-6xl md:text-5xl>
           Take Your Stocks To The Next Level
         </div>
-        <div class="pt-10 text-base md:text-lg">
+        <div pt-10 text-base md:text-lg>
           Fintasy is a stock market simulator that allows you to trade stocks with fake money. Compete against your
           friends and see who can make the most money!
         </div>
