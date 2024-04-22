cd1365f0e09d9e6f3555bb44fa80fac2dc6bc134
adibarra <93070681+adibarra@users.noreply.github.com>
2024-04-22 00:00:34 +0000

    remove (and invalidate) apca keys

--- packages/server/src/services/alpaca/alpaca.py
+++ packages/server/src/services/alpaca/alpaca.py
@@ -1,11 +1,8 @@
-# @Author: Omer Siddiqui 
+# @Author: Omer Siddiqui
 # File with helper function to retrieve stock info from Alpaca Markets API
 
 import requests
-#from config import APCA_API_KEY, APCA_API_SECRET
-
-APCA_API_KEY = 'PK6OU4VE4ULW9H037IJR'
-APCA_API_SECRET = 'B2shlnpi2YVCrAutPngoVZSbCInfG6citfu1HUuP'
+from config import APCA_API_KEY, APCA_API_SECRET
 
 # Alpaca API Endpoint
 api_host = "https://data.alpaca.markets/v2/stocks/trades"
@@ -13,12 +10,13 @@ api_host = "https://data.alpaca.markets/v2/stocks/trades"
 headers = {
     "APCA-API-KEY-ID": APCA_API_KEY,
     "APCA-API-SECRET-KEY": APCA_API_SECRET,
-    "accept": "application/json"
+    "accept": "application/json",
 }
+
+
 class AlpacaService:
-    
-    def get_latest_alpaca_quote(self, symbol:str):
-        """ Sends a GET Request to Alpaca API to retrieve latest quote"""
+    def get_latest_alpaca_quote(symbol: str):
+        """Sends a GET Request to Alpaca API to retrieve latest quote"""
         # Construct request url
         latest_url = f"{api_host}/latest?symbols={symbol}&feed=iex"
         print(latest_url)
@@ -34,28 +32,26 @@ class AlpacaService:
             price = response_data["trades"][symbol]["p"]
             # Get timestamp
             timestamp = response_data["trades"][symbol]["t"]
-            #print(timestamp)
+            # print(timestamp)
 
-            data = {
-                "symbol": symbol,
-                "price": price,
-                "timestamp": timestamp
-            }
+            data = {"symbol": symbol, "price": price, "timestamp": timestamp}
 
             return data
-            
+
         # Otherwise print error message
         else:
             print(f"Error: {response.status_code} - {response.text}")
             return None
 
-    def get_historical_alpaca_quote(self, symbol: str, start_time, end_time, quote_limit: int):
+    def get_historical_alpaca_quote(
+        symbol: str, start_time, end_time, quote_limit: int
+    ):
         """Sends GET request to Alpaca API to get the latest historical quotes"""
-        
+
         # Convert start and end time string to appropriate format for request
-        start_time = start_time.replace(':' , '%3A')
+        start_time = start_time.replace(":", "%3A")
         print(start_time)
-        end_time = end_time.replace(':' , '%3A')
+        end_time = end_time.replace(":", "%3A")
         print(end_time)
         # Construct request url
         historical_url = f"{api_host}?symbols={symbol}&start={start_time}&end={end_time}&limit={quote_limit}&feed=iex&currency=USD"
@@ -71,11 +67,10 @@ class AlpacaService:
         else:
             print(f"Error: {response.status_code} - {response.text}")
             return None
-        
+
+
 # Sample Tests
 """
-alpaca = AlpacaService()
-alpaca.get_latest_alpaca_quote("AAPL")
-
-alpaca.get_historical_alpaca_quote('AAPL', '2022-01-03T00:00:00Z', '2022-01-04T00:00:00Z', 10)
+AlpacaService.get_latest_alpaca_quote("AAPL")
+AlpacaService.get_historical_alpaca_quote('AAPL', '2022-01-03T00:00:00Z', '2022-01-04T00:00:00Z', 10)
 """
