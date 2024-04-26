d27bc797adc4fa87231f2e50f6487905d76f31e7
adibarra <93070681+adibarra@users.noreply.github.com>
2024-04-26 17:23:13 +0000

    begin implementing settings page and modals

--- packages/client/src/pages/dashboard/settings.vue
+++ packages/client/src/pages/dashboard/settings.vue
@@ -6,28 +6,42 @@
 <script setup lang="ts">
 const { t } = useI18n()
 
+const usernameModal = ref(false)
+
 useHead({
   title: `${t('pages.dashboard.settings.title')} • Fintasy`,
 })
 </script>
 
 <template>
-  {{ $t('example.hello-world') }}
-
-  <span>
-    Should be a nice settings page. It should look like the wireframe.
-  </span>
-
-  <div>
-    <LanguageSwitch />
+  <div h-full w-full flex flex-col gap-2 xl:flex-row>
+    <div flex grow flex-col fn-outline bg--c-fg p-2 max-xl:h-205>
+      <div mb-5 max-w-150 min-w-80 w-90svw flex flex-col gap-5 px-8 py-8>
+        <div flex gap-4>
+          Change Language: <LanguageSwitch />
+        </div>
+        <button
+          mt-5 fn-outline px-2 py-0.5 text-lg
+          @click="usernameModal = true"
+        >
+          Change Username
+        </button>
+        <button
+          mt-5 fn-outline px-2 py-0.5 text-lg
+          @click="() => {}"
+        >
+          Change Email
+        </button>
+        <button
+          mt-5 fn-outline px-2 py-0.5 text-lg
+          @click="() => {}"
+        >
+          Change Password
+        </button>
+      </div>
+    </div>
   </div>
-
-  <!-- Some spacers and a temporary footer -->
-  <span op-50>
-    <div h-10 />
-    [ pages/dashboard/settings.vue ]
-    <div h-10 />
-  </span>
+  <changeUsernameModal v-model="usernameModal" />
 </template>
 
 <route lang="yaml">
