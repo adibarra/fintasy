8978aeceb38eba266241046540ad0f938698515a
adibarra <93070681+adibarra@users.noreply.github.com>
2024-04-27 00:02:09 +0000

    clean up, add caching, fix response format

--- packages/server/src/services/alpaca/alpaca.py
+++ packages/server/src/services/alpaca/alpaca.py
@@ -1,12 +1,13 @@
-# @Author: Omer Siddiqui
-# File with helper function to retrieve stock info from Alpaca Markets API
+# @Author: adibarra (Alec Ibarra), omer8 (Omer Siddiqui)
+# @description: Helper function to retrieve stock info from Alpaca Markets API
+
+import datetime
+from dataclasses import asdict, dataclass
 
 import requests
 from config import APCA_API_KEY, APCA_API_SECRET
 
-# Alpaca API Endpoint
 api_host = "https://data.alpaca.markets/v2/stocks/trades"
-# Setup headers for JSON Request
 headers = {
     "APCA-API-KEY-ID": APCA_API_KEY,
     "APCA-API-SECRET-KEY": APCA_API_SECRET,
@@ -14,38 +15,58 @@ headers = {
 }
 
 
+@dataclass
+class Quote:
+    symbol: str
+    price_cents: int
+    timestamp: datetime.datetime
+
+
+CACHE_MAX_SIZE = 100
+CACHE: dict[str, Quote] = {}
+
+
 class AlpacaService:
-    def get_latest_alpaca_quote(symbol: str):
+    def get_quote(symbol: str) -> Quote | None:
         """Sends a GET Request to Alpaca API to retrieve latest quote"""
+        # Check if the symbol is in the cache
+        if symbol in CACHE:
+            quote = CACHE[symbol]
+            # Get current time
+            current_time = datetime.datetime.now()
+            # Get timestamp from cache
+            timestamp = quote.timestamp
+            # Calculate time difference
+            time_diff = current_time - timestamp
+            # Check if time difference is less than 15 minutes
+            if time_diff.total_seconds() < 900:
+                return quote
+
         # Construct request url
         latest_url = f"{api_host}/latest?symbols={symbol}&feed=iex"
-        print(latest_url)
-        # Create response object
         response = requests.get(latest_url, headers=headers)
 
         # Check if the request was successful
         if response.status_code == 200:
             response_data = response.json()
-            print(response_data)
 
-            # Get price from latest trades
-            price = response_data["trades"][symbol]["p"]
-            # Get timestamp
-            timestamp = response_data["trades"][symbol]["t"]
-            # print(timestamp)
+            price_cents = response_data["trades"][symbol]["p"] * 100
+            timestamp_str = response_data["trades"][symbol]["t"]
+            timestamp = datetime.datetime.strptime(
+                timestamp_str[:-4], "%Y-%m-%dT%H:%M:%S.%f"
+            )
 
-            data = {"symbol": symbol, "price": price, "timestamp": timestamp}
+            quote = Quote(symbol, price_cents, timestamp)
 
-            return data
+            # Add the quote to the cache
+            if len(CACHE) >= CACHE_MAX_SIZE:
+                CACHE.popitem()
+            CACHE[symbol] = asdict(quote)
+            return asdict(quote)
 
-        # Otherwise print error message
-        else:
-            print(f"Error: {response.status_code} - {response.text}")
-            return None
+        return None
 
-    def get_historical_alpaca_quote(
-        symbol: str, start_time, end_time, quote_limit: int
-    ):
+    def get_historical_quote(symbol: str, start_time, end_time, quote_limit: int):
         """Sends GET request to Alpaca API to get the latest historical quotes"""
 
         # Convert start and end time string to appropriate format for request
