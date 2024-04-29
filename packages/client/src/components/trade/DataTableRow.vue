7615ef639669e983c0bb7bbde520f871d985b15f
adibarra <93070681+adibarra@users.noreply.github.com>
2024-04-29 03:05:20 +0000

    reset qty after transaction

--- packages/client/src/components/trade/DataTableRow.vue
+++ packages/client/src/components/trade/DataTableRow.vue
@@ -32,6 +32,8 @@ async function createTransaction(action: ACTION) {
     action,
     quantity: quantity.value,
   })
+  quantity.value = 0
+
   // refresh the portfolios so that the new balance is loaded
   await state.refresh.portfolios()
 }
